072ab45bdaec0cfedda943f4fc81ac18b822b9b5
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-07-10 18:40:28 +0530

    replace number with dice icons

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"]
+  };
+
   constructor(props) {
     super(props);
 
@@ -13,14 +17,19 @@ class Die extends Component {
   }
 
   render() {
+    const { numberWords, locked, val, disabled } = this.props;
+
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+
+    if (locked) classes += " Die-locked"
+
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+      <i
+        className={classes}
         onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
+        disabled={disabled}
+      />
+
     );
   }
 }
